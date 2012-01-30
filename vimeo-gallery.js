19216f3b49345c376b74a5c4a0049c8e8fb53d75
Sam Margulies <freebuffalo@gmail.com>
2012-01-30 17:09:04 -0500

    initial commit

---
+++ vimeo-gallery.js
@@ -0,0 +1,69 @@
+jQuery(document).ready(function($){
+	var video_info = {};
+	
+	// Get the user's videos
+	function setupGalleries() {
+		
+		$('.vimeo-gallery .playlist li').each(function(){
+		
+			var video$ = $(this),
+				video_url = video$.attr('data-video-url');
+			
+			if( undefined == video_url ) {
+				return;
+			}
+			
+			$.ajax({
+			
+				'url' : 'http://vimeo.com/api/oembed.json?url=' + video_url + '&width=640&height=360&callback=addVideo',
+				'dataType' : 'jsonp',
+				'success' :  function(video){
+					// we have the embed info now, lets program the playlist
+					
+					// If we can't load the thumbnail, just give up
+					if( undefined == video.thumbnail_url ) {
+						return;
+					}
+					
+					console.log(video);
+					
+					// save all the the data for later
+					video_info[video.video_id] = video;
+					
+					var listItem = '<a href="##" data-video-id="' + video.video_id + '"><img src="' + video.thumbnail_url + '" class="thumbnail" />';
+					listItem += '<h2>' + video.title + '</h2></a><p>' +  video.description + '</p>';
+					
+					video$.html(listItem);
+					
+					if( video$.is('li:first-child') ) {
+						video$.find('a').click();
+					}
+				}
+			});
+			
+		});
+	}
+	
+	$('.vimeo-gallery .playlist a').live( 'click', function(e) {
+		e.preventDefault();
+		
+		var	currentListItem$ = $(this).parents('li'),
+			video_id = $(this).attr('data-video-id');
+
+		if( currentListItem$.hasClass('active') ) {
+			return;
+		}
+		
+		currentListItem$.siblings().removeClass('active');
+		currentListItem$.addClass('active');
+					
+		$(this).parents('.vimeo-gallery').find('.video').html( unescape(  video_info[ video_id ].html ) ).attr('data-video-id', video_id);
+		
+		return false;
+	});
+	
+	setupGalleries();
+
+});
+
+
---
+++ vimeo-gallery.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: Vimeo gallery shortcode
+Plugin URI: http://belabor.org/
+Description: Adds a [vimeo-gallery] shortcode
+Version: 1.0
+Author: Sam Margulies (http://belabor.org/)
+*/
+
+add_shortcode('vimeo-gallery', 'do_vimeo_gallery_shortcode');
+
+function do_vimeo_gallery_shortcode($args, $content="") {
+	
+	$defaults = array(
+		'width'  => 640,
+		'height' => 380	
+	);
+	
+	$args = wp_parse_args($args, $defaults);
+	
+	if( preg_match_all( '/http:\/\/vimeo\.com\/\d+/im', trim($content), $videos, PREG_PATTERN_ORDER) == 0 ) {
+		echo "<!-- Vimeo gallery failed to find any videos -->";
+		return;
+	}
+	
+	$videos = $videos[0];
+	
+	static $gallery_id;
+	$gallery_id++;
+
+	wp_enqueue_style( 'vimeo_gallery_css', plugins_url( 'vimeo-gallery.css' , __FILE__ ) );
+	wp_enqueue_script( 'vimeo_gallery_scripts', plugins_url( 'vimeo-gallery.js' , __FILE__ ), array('jquery') );
+
+	?>
+
+	<div id="vimeo-gallery-<?php echo $gallery_id; ?>" class="vimeo-gallery" style="width:<?php echo $args['width']; ?>px; height: <?php echo $args['height']; ?>px;">
+		<div class="video"></div>
+		<div class="playlist">
+			<ul>
+			<?php
+			foreach($videos as $video) {
+				$video = esc_attr( strtolower($video) );
+				echo "<li data-video-url='$video'/></li>";
+			}
+			?>	
+			</ul>
+		</div>
+	</div>
+
+	<?php
+}
+
+?>
